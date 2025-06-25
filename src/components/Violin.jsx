afd815d3184162c31da44f2ba15f2101b10c6906
unknown <elderculquig>
2025-06-25 16:56:28 -0500

    New component

---
+++ src/components/Violin.jsx
@@ -0,0 +1,14 @@
+export default function Guitar({ guitar, addToCard }) {
+    return (
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+            </div>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">Violin ejemplo</h3>
+                <p>Description</p>
+                <p className="fw-black text-primary fs-3">$ 00.00</p>
+            </div>
+        </div>
+    )
+}
