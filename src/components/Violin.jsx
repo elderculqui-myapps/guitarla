b940318ed0052cfb8a2ae4cd0f29322719accce2
ElderCulqui <elderculquig@gmail.com>
2025-06-25 17:15:11 -0500

    Add var for component

--- src/components/Violin.jsx
+++ src/components/Violin.jsx
@@ -1,13 +1,17 @@
 export default function Violin() {
+    const name = 'Violin';
+    const description = 'This is a sample violin description.';
+    const price = '12.00';
+
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
                 <span>Image</span>
             </div>
             <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">Violin ejemplo</h3>
-                <p>Description</p>
-                <p className="fw-black text-primary fs-3">$ 00.00</p>
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-black text-primary fs-3">$ {price}</p>
             </div>
         </div>
     )
